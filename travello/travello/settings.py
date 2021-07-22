915658f87d1d2f679ee4c30edb96ff1cf0eafabd
DJoshBrown <81064467+DJoshBrown@users.noreply.github.com>
2021-07-22 10:01:42 -0400

    Removal of Secret Key
    'SECRET_KEY' has been removed, and replaced with access to an environmental variable.
    'DEBUG' has been changed to 'FALSE'.

--- travello/travello/settings.py
+++ travello/travello/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&rvaoe8j-koe2j6@wh_4gaicpl4-@f9)!9)7b)-up-ehb&c4+='
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
